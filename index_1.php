103687df939f4814fe01c2938a228f1f7c0048e9
Alla Belhafsi <alla.belhafsi@stagiaire-elan.fr>
2023-11-21 11:49:13 +0100

    first commit

---
+++ index_1.php
@@ -0,0 +1,29 @@
+<!-- OBJECTIF 1 --- La liste des gaulois triés dans l'ordre alphabétique du nom du personnage -->
+
+<?php
+try
+{
+	// On se connecte à MySQL
+	$mysqlClient = new PDO('mysql:host=localhost;dbname=gaulois;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+
+// Si tout va bien, on peut continuer
+
+// On récupère tout le contenu de la table recipes
+$sqlQuery = 'SELECT personnage.nom_personnage FROM personnage ORDER BY nom_personnage ASC';
+$personnageStatement = $mysqlClient->prepare($sqlQuery);
+$personnageStatement->execute();
+$personnages = $personnageStatement->fetchAll();
+
+// On affiche chaque recette une à une
+foreach ($personnages as $personnageData) {
+?>
+    <p><?php echo $personnageData['nom_personnage']; ?></p>
+<?php
+}
+?>
---
+++ index_2.php
@@ -0,0 +1,48 @@
+<!-- OBJECTIF 1 --- La liste des villages avec le nombre d'habitants de chaque village (un tableau HTML à 2 colonnes: nom du lieu / nombre d'habitants) -->
+
+<?php
+try
+{
+	// On se connecte à MySQL
+	$mysqlClient = new PDO('mysql:host=localhost;dbname=gaulois;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+
+// Si tout va bien, on peut continuer
+
+// On récupère tout le contenu de la table recipes
+$sqlQuery = 'SELECT 
+                 lieu.nom_lieu, 
+                 COUNT(personnage.id_personnage) AS nbHabitants 
+            FROM lieu
+            INNER JOIN personnage ON lieu.id_lieu = personnage.id_lieu
+            GROUP BY lieu.id_lieu
+            ORDER BY nbHabitants ASC';
+$lieuStatement = $mysqlClient->prepare($sqlQuery);
+$lieuStatement->execute();
+$lieux = $lieuStatement->fetchAll();
+
+// Affichage dans un tableau HTML à deux colonnes
+?>
+<table>
+    <thead>
+        <tr>
+            <th style="text-align: center;">Nom du lieu</th>
+            <th style="text-align: center;">Nombre d'habitants</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($lieux as $lieuData) { ?>
+            <tr>
+                <td><?php echo $lieuData['nom_lieu']; ?></td>
+                <td style="text-align: center;"><?php echo $lieuData['nbHabitants']; ?></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
+
+
